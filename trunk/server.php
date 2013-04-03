f2a788fa0b7829b5f2056c4339bbbc4df99096fc
devsoftcreations <devsoftcreations@c124db7b-2890-68c7-eaeb-c76a59950744>
2013-04-03 07:15:05 +0000

    I have create 3 pages for domain and webhosting registration.

---
+++ trunk/server.php
@@ -0,0 +1,23 @@
+<?php
+// Pull in the NuSOAP code
+require_once('nusoap.php');
+// Create the server instance
+$server = new soap_server;
+// Register the method to expose
+$server->configureWSDL('taxiserver', 'urn:taxiupload');
+$server->register('hello');
+$server->register("hello",
+array('name' => 'xsd:string'),
+array('return' => 'xsd:string'),
+'urn:taxiupload',
+'urn:taxiupload#hello');
+
+// Define the method as a PHP function
+function hello($name) 
+{
+    return 'Hello, ' . $name;
+}
+// Use the request to (try to) invoke the service
+$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+$server->service($HTTP_RAW_POST_DATA);
+?>
